581e276615622be21de3e7e023058ef9779581e6
Sam Graham <git@illusori.co.uk>
2012-09-15 05:06:21 +0100

    Add LICENSE.txt.

--- jquery.transformlist.js
+++ jquery.transformlist.js
@@ -1,3 +1,9 @@
+/* Copyright 2012, Sam Graham.
+ * Licensed under GPL version 3.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ * For latest version, see https://github.com/illusori/jquery.transformlist
+ */
 (function (window, $, undefined) {
 
 "use strict";
